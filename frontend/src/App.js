4a227041f4ed93b045f4b1341f61c06857a5ada1
izzahnin <nurhidayatni21d@student.unhas.ac.id>
2023-04-09 23:09:52 +0800

    copas fe sebelumnya

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProductList from "./components/ProductList";
+import AddProduct from "./components/AddProduct";
+import EditProduct from "./components/EditPruduct";
+import Navbar from "./components/Navbar";
+import Home from './pages/Home';
+import AboutUs from './pages/AboutUs';
+import ContactUs from './pages/ContactUs';
+import Products from './pages/Products';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' exact Component={Home}/>
+          <Route path='/products' exact Component={Products}/>
+          <Route path='/aboutus' exact Component={AboutUs}/>
+          <Route path='/contactus' exact Component={ContactUs}/>
+
+          <Route path="/" element={<ProductList />} />
+          <Route path="add" element={<AddProduct />} />
+          <Route path="edit/:id" element={<EditProduct />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
